a31d9e1ce6f77c0f8c12567d467006ac1c19653a
Cody <cknapp3@student.cvtc.edu>
2015-04-12 04:43:35 +0000

    Update create_properties table update db schema

--- propertyRating/db/migrate/20150408160427_create_properties.rb
+++ propertyRating/db/migrate/20150408160427_create_properties.rb
@@ -2,7 +2,7 @@ class CreateProperties < ActiveRecord::Migration
   def change
     create_table :properties do |t|
       t.string :address
-
+     
       t.timestamps null: false
     end
   end
--- propertyRating/db/schema.rb
+++ propertyRating/db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408160427) do
+ActiveRecord::Schema.define(version: 20150412043212) do
 
   create_table "properties", force: :cascade do |t|
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "apt"
+    t.string   "city"
+    t.string   "state"
+    t.integer  "zip"
   end
 
 end
