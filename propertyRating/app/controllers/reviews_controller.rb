b6684ec33a49b7c31794257ab000c314c81529b7
Jerry and Adam <Adam.Papierniak@gmail.com>
2015-04-22 19:36:34 -0500

    Add in the code that enables comments

--- propertyRating/app/controllers/reviews_controller.rb
+++ propertyRating/app/controllers/reviews_controller.rb
@@ -1,14 +1,21 @@
 class ReviewsController < ApplicationController
+    before_action :findProperty
+    
     def create
-        @property = Property.find(params[:property_id])
         @review = @property.reviews.create(params.require(:review).permit!)
         redirect_to @property
     end
     
-    def destroy
-        @property = Property.find(params[:property_id])
+    def show
         @review = @property.reviews.find(params[:id])
+    end
+    
+    def destroy
         @review.destroy
         redirect_to @property
+    end
+    
+    def findProperty
+        @property = Property.find(params[:property_id])
     end
 end
