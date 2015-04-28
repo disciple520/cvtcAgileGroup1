61ee3c57f445e7d0de96a7e7276a478bb87d35f1
Mark Allman <allmanmarks@gmail.com>
2015-04-28 01:50:08 +0000

    add name of controller for callbacks

--- propertyRating/config/routes.rb
+++ propertyRating/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
     resources :properties do
         resources :reviews do
             resources :comments
