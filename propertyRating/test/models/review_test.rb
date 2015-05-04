08116030ba94043e6e4a8e0c2d3862fde6709ea8
Cody <cknapp3@student.cvtc.edu>
2015-05-04 19:19:48 +0000

    add average rating test

--- propertyRating/test/models/review_test.rb
+++ propertyRating/test/models/review_test.rb
@@ -5,4 +5,11 @@ class ReviewTest < ActiveSupport::TestCase
   test_review = Review.create(rating:"1", explanation:"Blah")
   assert test_review.rating = "1"
   end  
+  
+  test " average rating" do
+    test_review1 = Review.create(rating:'1', explanation:"gross")
+    test_review2 = Review.create(rating:'5', explanation:"not gross")
+    test_average = (test_review1.rating + test_review2.rating) / 2
+    assert test_average = "3"
+  end
 end
